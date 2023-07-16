6fe320a55bcf14427079d82c6ad8c46729da7a3b
Olga <114821334+EfimovaOl@users.noreply.github.com>
2023-07-16 17:52:44 +0300

    Add files via upload

---
+++ Lesson 4.py
@@ -0,0 +1,38 @@
+# Напишите функцию для транспонирования матрицы
+
+def transpose(table):
+    table2 = []
+
+    for i in range(len(table[0])):
+        table2.append(list())
+        for j in range(len(table)):
+            table2[i].append(table[j][i])
+        return table2
+
+matrix = [[1, 2, 3], [4, 5, 6]]
+print(matrix)
+matrix2 = transpose(matrix)
+print(matrix2)
+
+
+# Напишите функцию принимающую на вход только ключевые параметры и возвращающую словарь,
+# где ключ — значение переданного аргумента, а значение — имя аргумента. Если ключ не хешируем,
+# используйте его строковое представление.
+
+def dictionary(one, two):
+     dict = {
+         f'{one}': id(one),
+         f'{two}': id(two)
+     }
+     return dict
+a = 1
+b = 2
+dic = dictionary(a, b)
+print(dic)
+
+
+
+# Возьмите задачу о банкомате из семинара
+# 2. Разбейте её на отдельные операции — функции.
+# Дополнительно сохраняйте все операции поступления и снятия средств в список.
+
